41862a8ea95f4a3d46e1bacffd973278da44d6f0
Chupeng Shen <shenchupeng0807@gmail.com>
2020-10-02 02:22:42 -0400

    Test Case for Game Simple

---
+++ Pirates/src/main/GamePlayTest.java
@@ -0,0 +1,15 @@
+package main;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import junit.framework.TestCase;
+
+
+//Unit test for hole game
+public class GamePlayTest extends TestCase
+{
+	GameServer GS = new GameServer();
+
+}
---
+++ Pirates/src/main/StepDefSinglePlayerClient.java
@@ -0,0 +1,14 @@
+package main;
+
+import org.junit.jupiter.api.Test;
+
+import junit.framework.TestCase;
+
+public class StepDefSinglePlayerClient extends TestCase{
+
+	public void test() 
+	{
+		
+	}
+
+}
